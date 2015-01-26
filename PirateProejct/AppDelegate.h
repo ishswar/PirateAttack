f5736ec1b201bd63f646bd35c5019468dc331c09
Pranay Shah <Pranay@pranays-mbp.attlocal.net>
2015-01-25 21:54:29 -0800

    Initial Commit

---
+++ PirateProejct/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PirateProejct
+//
+//  Created by Pranay Shah on 1/25/15.
+//  Copyright (c) 2015 Pranay Shah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PirateProejct/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PirateProejct
+//
+//  Created by Pranay Shah on 1/25/15.
+//  Copyright (c) 2015 Pranay Shah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
